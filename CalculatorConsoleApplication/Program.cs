173b2f6afefa667b27e825461fd841fe3e3a2e3c
sabarirajp <sabarirajpazhani@gmail.com>
2025-04-23 18:19:01 +0530

    Add project files.

---
+++ CalculatorConsoleApplication/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CalculatorConsoleApplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************** CALCULATOR ****************");
+            Console.WriteLine("|                                          |");
+            Console.WriteLine("------ Enter the number with Operator ------");
+            int result = Convert.ToInt32(Console.ReadLine());
+            
+            while (true)
+            {
+                string operators = Console.ReadLine();
+
+                if (operators == "=")
+                {
+                    Console.WriteLine("------ Result: " + result + "------");
+                    break;
+                }
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                switch (operators)
+                {
+                    case "+":
+                        result += num;
+                        break;
+                    case "-":
+                        result += num;
+                        break;
+                    case "*":
+                        result *= num;
+                        break;
+                    case "/":
+                        result/= num;
+                        break;
+                    case "%":
+                        result *= num;
+                        break;
+                        
+                }
+
+            }
+        }
+    }
+}
