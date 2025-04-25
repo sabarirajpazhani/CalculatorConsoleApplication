ff887efd5c2a1f6fce3052bcc6f7e181585a8316
sabarirajp <sabarirajpazhani@gmail.com>
2025-04-25 18:54:35 +0530

    comnsole application

--- CalculatorConsoleApplication/Program.cs
+++ CalculatorConsoleApplication/Program.cs
@@ -9,20 +9,28 @@ namespace CalculatorConsoleApplication
             Console.WriteLine("**************** CALCULATOR ****************");
             Console.WriteLine("|                                          |");
             Console.WriteLine("------ Enter the number with Operator ------");
-            
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("Want to view the final Result press = to get the result");
+            Console.WriteLine("Want to exit, Enter 'e' ");
+
             int result = Convert.ToInt32(Console.ReadLine());
-            
+
             while (true)
             {
                 string operators = Console.ReadLine();
 
                 if (operators == "=")
                 {
-                    Console.WriteLine("----------- Result: " + result + "----------");
+                    Console.WriteLine("--------------- Result: " + result + "---------------");
+                    continue; 
+                }
+
+                if (operators == "e")
+                {
+                    Console.WriteLine("--------------- Final Result: " + result + "---------------");
                     break;
                 }
                 int num = Convert.ToInt32(Console.ReadLine());
-
                 switch (operators)
                 {
                     case "+":
@@ -35,14 +43,12 @@ namespace CalculatorConsoleApplication
                         result *= num;
                         break;
                     case "/":
-                        result/= num;
+                        result /= num;
                         break;
                     case "%":
                         result %= num;
                         break;
-                        
                 }
-
             }
         }
     }
